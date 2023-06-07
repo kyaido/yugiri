b930b9280b8af0906349560b9e95687aec2f4990
kyaido <--local>
2023-06-07 10:19:15 +0900

    update trash

--- server/src/trash/trash.service.ts
+++ server/src/trash/trash.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
-import log from '../util/log';
 
 import moment from 'moment';
 import trash from 'trash';
@@ -12,9 +11,7 @@ export class TrashService {
   @Cron(CronExpression.EVERY_DAY_AT_6AM, { timeZone: 'Asia/Tokyo' })
   handleCron() {
     const date = moment().format('YYMMDD');
-    trash(['dist/public/optimized/*', `!dist/public/optimized/${date}`]).then(() => {
-      log('Trash old images.');
-    });
+    trash(['dist/public/optimized/*', `!dist/public/optimized/${date}`]);
     this.logger.debug('Trash old images.');
   }
 }
